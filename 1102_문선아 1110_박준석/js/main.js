4d48d5a769b51d1063cfe2454bd2483c88df3c01
ParkJunSeok <jdksla0129@gmail.com>
2025-06-25 19:53:45 +0900

    진짜 완성인건가?

--- 1102_문선아 1110_박준석/js/main.js
+++ 1102_문선아 1110_박준석/js/main.js
@@ -1,6 +1,6 @@
 const b_left = window.screenX;
 const b_top = window.screenY;
-// 팝업창2
+// 팝업창2 웹사이트 안쪽에서 열리도록 설계
 let newWin = window.open("popup.html", "123", `width = 500, height = 300, left=${b_left + 100}, top = ${b_top + 100}`)
 
 
@@ -8,7 +8,7 @@ const popupId = document.getElementById("popupId");
 const img = document.getElementById("img");
 const popupClose = document.getElementById("close");
 
-
+// 함수사용
 window.onload = function () {
   popupId.style.display = 'block'; 
   img.src = '../image/image7.png'
@@ -21,7 +21,7 @@ popupClose.onclick = function () {
   popupId.style.display = 'none';
 };
 
-// 제목과 메뉴 사진이 보이도록 설계
+// 제목과 메뉴 사진이 항상 보이도록 설계
 function firstpage(page){
     document.getElementById('contentFrame').src = page;
 }
